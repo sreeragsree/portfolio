2b0a17e39804dd82bf591fcb0f0b811537865087
Sreerag Murali <sreeragm46@gmail.com>
2024-08-03 03:12:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     rollupOptions: {
+       treeshake: false,
       output: {
         manualChunks: {
           three: ["three"], // Split three.js into its own chunk
